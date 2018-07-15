e4332f009730eadb0d8d8efc9137acec3f3670f7
柳涛 <935486956@qq.com>
2018-07-15 11:09:10 +0800

    first push

---
+++ components/IFRender/index.en-US.md
@@ -0,0 +1,20 @@
+---
+category: Components
+type: Other
+subtitle: 分支判断
+title: IFRender
+---
+
+分支判断
+
+## 何时使用
+
+- 需要 `if...else` 判断渲染组件时
+
+## API
+
+| 参数 | 说明 | 类型 | 默认值 |
+| --- | --- | --- | --- |
+| condition | 布尔变量 | Boolea | false |
+| then | 为 `true` 时渲染的组件 | Element | - |
+| else | 为 `false` 时渲染的组件 | Element | - |
---
+++ site/theme/template/Layout/index.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { enquireScreen } from 'enquire-js';
+import { addLocaleData, IntlProvider } from 'react-intl';
+import 'moment/locale/zh-cn';
+import { LocaleProvider } from 'antd';
+import 'antd/dist/antd.css';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import OfflineRuntime from 'offline-plugin/runtime';
+import Header from './Header';
+import Footer from './Footer';
+import enLocale from '../../en-US';
+import cnLocale from '../../zh-CN';
+import * as utils from '../utils';
+
+if (typeof window !== 'undefined') {
+  OfflineRuntime.install();
+}
+
+if (typeof window !== 'undefined') {
+  /* eslint-disable global-require */
+  require('../../static/style');
+
+  // Expose to iframe
+  window.react = React;
+  window['react-dom'] = ReactDOM;
+  window.antd = require('antd');
+  /* eslint-enable global-require */
+}
+
+let isMobile = false;
+enquireScreen((b) => {
+  isMobile = b;
+});
+
+export default class Layout extends React.Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired,
+  }
+
+  static childContextTypes = {
+    isMobile: PropTypes.bool,
+  };
+
+  getChildContext() {
+    const { isMobile: mobile } = this.state;
+    return { isMobile: mobile };
+  }
+
+  constructor(props) {
+    super(props);
+    const { pathname } = props.location;
+    const appLocale = utils.isZhCN(pathname) ? cnLocale : enLocale;
+    addLocaleData(appLocale.data);
+
+    this.state = {
+      appLocale,
+      isMobile,
+    };
+  }
+
+  componentDidMount() {
+    const { router } = this.context;
+    router.listen((loc) => {
+      if (typeof window.ga !== 'undefined') {
+        window.ga('send', 'pageview', loc.pathname + loc.search);
+      }
+      // eslint-disable-next-line
+      if (typeof window._hmt !== 'undefined') {
+        // eslint-disable-next-line
+        window._hmt.push(['_trackPageview', loc.pathname + loc.search]);
+      }
+    });
+
+    const nprogressHiddenStyle = document.getElementById('nprogress-style');
+    if (nprogressHiddenStyle) {
+      this.timer = setTimeout(() => {
+        nprogressHiddenStyle.parentNode.removeChild(nprogressHiddenStyle);
+      }, 0);
+    }
+
+    enquireScreen((b) => {
+      this.setState({
+        isMobile: !!b,
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timer);
+  }
+
+  render() {
+    const { children, ...restProps } = this.props;
+    const { appLocale } = this.state;
+
+    return (
+      <IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+        <LocaleProvider locale={appLocale.locale === 'zh-CN' ? zhCN : null}>
+          <div className="page-wrapper">
+            <Header {...restProps} />
+            {children}
+            <Footer {...restProps} />
+          </div>
+        </LocaleProvider>
+      </IntlProvider>
+    );
+  }
+}
